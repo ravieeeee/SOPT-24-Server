8ff0a7ddaeafc9d66714ab9c2787b17afd03cef5
JuYeong <pyeonjy97@gmail.com>
2019-04-16 23:27:34 +0900

    중복제거

--- hw/juyeongpyeon_lab_2_1.js
+++ hw/juyeongpyeon_lab_2_1.js
@@ -37,7 +37,6 @@ const server = http.createServer((req, res) => {
         msg: 'success',
         hashed
       }
-      res.writeHead(200, { 'Content-Type': 'application/json' })
       res.end(JSON.stringify(result))
     })
   })
