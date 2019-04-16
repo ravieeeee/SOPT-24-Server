bd0bb548f40c74fd0a513de8c791109b319a70a6
JuYeong <pyeonjy97@gmail.com>
2019-04-16 23:25:28 +0900

    hw1 에러처리

--- hw/juyeongpyeon_lab_2_1.js
+++ hw/juyeongpyeon_lab_2_1.js
@@ -4,14 +4,32 @@ const querystring = require('querystring')
 const crypto = require('crypto')
 
 const server = http.createServer((req, res) => {
+  const errResult = {
+    msg: 'error',
+    hashed: {}
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+
   const query = querystring.parse(url.parse(req.url).query)
+  if (!query.str) {
+    res.end(JSON.stringify(errResult))
+    return
+  }
   console.log('query:', query)
 
   crypto.randomBytes(64, (err, buf) => {
-    if (err) return
+    if (err) {
+      res.end(JSON.stringify(errResult))
+      return
+    }
 
     const salt = buf.toString('base64')
     crypto.pbkdf2(query.str, salt, 100, 64, 'SHA512', (err, derivedKey) => {
+      if (err) {
+        res.end(JSON.stringify(errResult))
+        return
+      }
+
       const hashed = derivedKey.toString('base64')
       console.log('hashed str:', hashed)
 
