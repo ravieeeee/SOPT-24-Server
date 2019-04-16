984be99b9488151c0ca0c682f3c3a262f36de7a0
JuYeong <pyeonjy97@gmail.com>
2019-04-17 00:34:55 +0900

    prac2

---
+++ prac/juyeongpyeon_lab_2_2.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const request = require('request')
+const { Parser } = require('json2csv')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  const uri = 'http://15.164.75.18:3000/homework/2nd'
+  request.get(uri, (err, response, body) => {
+    console.log('body:', body)
+    
+    const data = JSON.parse(body).data
+    console.log('data:', data.toString())
+
+    const json2csvParser = new Parser({ fields: ['datetime'] })
+    const csv = json2csvParser.parse([{
+      'datetime': data,
+    }])
+    fs.writeFile('./data.csv', csv, (err) => {
+      if (err) return
+      res.end()
+    })
+  })
+})
+
+server.listen(3000)
