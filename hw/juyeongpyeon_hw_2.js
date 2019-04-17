b73472d81d62540caaa12e8037489219e191f5e2
JuYeong <pyeonjy97@gmail.com>
2019-04-17 22:52:40 +0900

    signup

--- hw/juyeongpyeon_hw_2.js
+++ hw/juyeongpyeon_hw_2.js
@@ -4,6 +4,7 @@ const querystring = require('querystring')
 const crypto = require('crypto')
 const { Parser } = require('json2csv')
 const fs = require('fs')
+const csvtojson = require('csvtojson')
 
 const server = http.createServer((req, res) => {
   switch (url.parse(req.url).pathname) {
@@ -47,6 +48,35 @@ const server = http.createServer((req, res) => {
       break
     }
     case '/signup': {
+      const query = querystring.parse(url.parse(req.url).query)
+      if (!query.id || !query.pw) {
+        res.end('error: invalid input')
+        return
+      }
+      console.log('query:', query)
+      
+      let salt = undefined
+      csvtojson().fromFile('./userData.csv')
+      .then((jsonObj) => {
+        salt = jsonObj[0].salt
+        savedPW = jsonObj[0].pw
+        console.log('salt', salt)
+
+        crypto.pbkdf2(query.pw, salt, 100, 64, 'SHA512', (err, derivedKey) => {
+          if (err) {
+            res.end('error: pbkdf2')
+            return
+          }
+    
+          const hashedPW = derivedKey.toString('base64')
+          res.writeHead(200, { 'Content-Type':'text/plain; charset=utf-8' })
+          if (hashedPW === savedPW) {
+            res.end('로그인 성공')
+          } else {
+            res.end('비밀번호가 다릅니다.')
+          }
+        })
+      })
       break
     }
     case '/info': {
